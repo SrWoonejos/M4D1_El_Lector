64a536b9695be4b370dcfab5c4627b62f0d8b053
Daniela Miranda <128627388+SrWoonejos@users.noreply.github.com>
2023-10-04 19:48:08 -0300

    avance2

--- app/src/main/java/com/dmiranda/ellector_m4d1/ScrollingActivity.kt
+++ app/src/main/java/com/dmiranda/ellector_m4d1/ScrollingActivity.kt
@@ -1,11 +1,9 @@
 package com.dmiranda.ellector_m4d1
 
 import android.os.Bundle
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.dmiranda.ellector_m4d1.databinding.ActivityScrollingBinding
+import com.google.android.material.snackbar.Snackbar
 
 class ScrollingActivity : AppCompatActivity() {
 
