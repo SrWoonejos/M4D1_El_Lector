3066e95488bf2c3b0fe3b2625d8891b775f164bb
Daniela Miranda <128627388+SrWoonejos@users.noreply.github.com>
2023-10-04 19:22:18 -0300

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ElLector_M4D1"
+include(":app")
